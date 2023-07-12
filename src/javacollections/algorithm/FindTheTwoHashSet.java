efeec46f5388ccc1fb4377a75ec46a80e0964ff2
Lê Bảo Đăng <85282838+LeBaoDang@users.noreply.github.com>
2023-07-12 13:51:21 +0700

    .

---
+++ src/javacollections/algorithm/FindTheTwoHashSet.java
@@ -0,0 +1,16 @@
+package javacollections.algorithm;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class FindTheTwoHashSet {
+    public static void main(String[] args) {
+        HashSet<String> wildAnimal = new HashSet<>();
+        Scanner sr = new Scanner(System.in);
+        String input1 = sr.nextLine();
+        String input2 = sr.nextLine();
+        String input3 = sr.nextLine();
+
+        sr.close();
+    }
+}
