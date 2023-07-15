54304ca2ed0cf529d129b911564d999901aa501d
Lê Bảo Đăng <85282838+LeBaoDang@users.noreply.github.com>
2023-07-15 17:17:45 +0700

    .

--- src/javaexception/TryCatch.java
+++ src/javaexception/TryCatch.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class TryCatch {
     public static void main(String[] args) {
         Scanner sr = new Scanner(System.in);
-
         try {
             int numbers[] = {10,20,30};
             int index = sr.nextInt();
